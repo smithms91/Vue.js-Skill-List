658bcfb974953c06b552a4288315beac1a8e852b
Michael Smith <smithms91@gmail.com>
2018-07-02 11:08:33 -0400

    First Vue.js project - Skill List

---
+++ src/components/About.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="about">
+    <h1>Hello {{their_name}}, this is an about page.</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod temport incididunt</p>
+  </div>  
+</template>
+
+<script>
+export default {
+  name: 'About',
+  data() {
+    return {
+      their_name: this.$route.params.name
+    }
+  }
+}
+</script>
+
+
+
